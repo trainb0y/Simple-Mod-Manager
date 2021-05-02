882d99b2c836e069c68a097fdf16fc11ae99f08e
trainb0y1 <btbevo00@gmail.com>
2021-05-02 10:20:09 -0600

    Add settings window for directories

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
 import tkinter as tk
 import os, shutil, json
 import tkinter.messagebox
+from tkinter import filedialog
 
-df_dir = os.path.dirname(os.path.abspath(__file__))
-mod_folder = os.path.join(df_dir,"mods")
+
+
+while True:
+    try: 
+        f= open(os.path.join(os.path.dirname(os.path.abspath(__file__)),"smm_settings.json"), "r")
+        settings = json.load(f)
+        f.close()
+        temp = tk.Tk() # Have to have a base window for the messageboxes
+        temp.withdraw() # Make it dissapear
+        try: 
+            df_dir = settings["DF Dir"]
+        except KeyError:
+            tkinter.messagebox.showwarning("Warning","A Dwarf Fortress folder has not been selected! \nPlease select it in Settings\n Defaulting to program directory")
+            df_dir = os.path.dirname(os.path.abspath(__file__))
+        try: 
+            mod_dir = settings["Mod Dir"]
+        except KeyError:
+            tkinter.messagebox.showwarning("Warning","A Mods folder has not been selected! \nPlease select it in Settings\n Defaulting to a folder \"mods\" in the DF Main Directory")
+            mod_dir = os.path.join(df_dir,"mods")
+        temp.destroy() # Get rid of the temporary tk, as we create a new one later.
+        break
+
+    except FileNotFoundError: 
+        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),"smm_settings.json"), "x") as f:
+            json.dump({},f)
+        # Create the file and try again
+    
+    
+
 
 def merge_dirs(root_src_dir,root_dst_dir): # Thank you StackOverflow!
     for src_dir, dirs, files in os.walk(root_src_dir):
@@ -21,6 +49,63 @@ def merge_dirs(root_src_dir,root_dst_dir): # Thank you StackOverflow!
             shutil.copy(src_file, dst_dir) # Was move, I changed this hopefully didn't break anything
 
 
+class SettingsWindow(tk.Toplevel):
+    def __init__(self,master):
+        super().__init__(master)
+        self.master = master
+        self.title("Settings")
+        self.grid()
+        self.create_widgets()
+    
+    def create_widgets(self):
+        self.df_dir_label = tk.Label(self,text="Main Dwarf Fortress Folder")
+        self.df_dir_label.grid(row=0,column=0)
+        self.df_dir_strvar = tk.StringVar(self)
+        self.df_dir_strvar.set("")
+        self.df_dir_entry = tk.Entry(self,width=40,textvariable=self.df_dir_strvar)
+        self.df_dir_entry.grid(row=0,column=1)
+        self.df_dir_browse_button = tk.Button(self,text="Browse",
+            command=lambda: self.df_dir_strvar.set(filedialog.askdirectory(title="Main Dwarf Fortress Folder")))
+        self.df_dir_browse_button.grid(row=0,column=3)
+
+
+        self.mod_dir_label = tk.Label(self,text="Mods Folder")
+        self.mod_dir_label.grid(row=1,column=0)
+        self.mod_dir_strvar = tk.StringVar(self)
+        self.mod_dir_strvar.set("")
+        self.mod_dir_entry = tk.Entry(self,width=40,textvariable=self.mod_dir_strvar)
+        self.mod_dir_entry.grid(row=1,column=1)
+        self.mod_dir_browse_button = tk.Button(self,text="Browse",
+            command=lambda: self.mod_dir_strvar.set(filedialog.askdirectory(title="Mods Folder")))
+        self.mod_dir_browse_button.grid(row=1,column=3)
+
+        # Put the current directories in the Entrys
+        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),"smm_settings.json"), "r") as f:
+            settings = json.load(f)
+            try:
+                self.mod_dir_strvar.set(settings["Mod Dir"])
+                self.df_dir_strvar.set(settings["DF Dir"])
+            except KeyError:
+                pass # KeyError if they haven't selected it yet.
+                    # Without this it never renders the buttons
+
+
+        self.apply_button = tk.Button(self,text="Apply",command=self.apply_settings)
+        self.apply_button.grid(row=2,column=0,ipadx=30)
+
+        self.cancel_button = tk.Button(self,text="Cancel",command=self.destroy)
+        self.cancel_button.grid(row=2,column=1,ipadx=30)
+    
+    def apply_settings(self):
+        global df_dir, mod_dir
+        df_dir = self.df_dir_strvar.get()
+        mod_dir = self.mod_dir_strvar.get()
+        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),"smm_settings.json"), "w") as f:
+            json.dump({"DF Dir":df_dir,"Mod Dir":mod_dir},f) # Save the json
+
+        tkinter.messagebox.showinfo("Success","Changes have been applied")
+        
+
 
 class MainWindow(tk.Frame):
     def __init__(self,master=None):
@@ -32,8 +117,11 @@ class MainWindow(tk.Frame):
     
     def create_widgets(self):
        
-        self.help_button = tk.Button(self,text="Help",command=lambda: tkinter.messagebox.showinfo("Help",'This program can help with the loading and unloading of mods for Dwarf Fortress. This file should be placed in the main DF directory, along with a folder called "mods" (without the quotes) that contains all of the individual mod folders. Mod folders should contain a folder called "raw" (again, without the quotes) that contains all of the files to load in. Anything outside of the raw folder is ignored. \n\nPlease do not delete mod_info.json, as doing such will cause whatever is currently in the DF raw folder to become the default. (Well, unless you want it to be)\n\nTHERE IS NO GUARANTEE THAT THIS WILL NOT DESTROY ALL RAWS'))
-        self.help_button.grid(row=0,column=0,columnspan=2,ipadx=30)
+        self.help_button = tk.Button(self,text="Help",command=lambda: tkinter.messagebox.showinfo("Help",'This program can help with the loading and unloading of mods for Dwarf Fortress. Mod folders should go in a mods folder, selected in the settings. Mod folders should contain a folder called "raw" (again, without the quotes) that contains all of the files to load in. Anything outside of the raw folder is ignored. \n\nPlease do not delete mod_info.json, as doing such will cause whatever is currently in the DF raw folder to become the default. (Well, unless you want it to be)\n\nTHERE IS NO GUARANTEE THAT THIS WILL NOT DESTROY ALL RAWS'))
+        self.help_button.grid(row=0,column=0,ipadx=30)
+
+        self.settings_button = tk.Button(self,text="Settings",command=lambda: SettingsWindow(self.master))
+        self.settings_button.grid(row=0,column=1,ipadx=30)
 
         self.loaded_mods_labelframe = tk.LabelFrame(self,text="Loaded Mods",bd=6) # bd is border thickness
         self.loaded_mods_labelframe.grid(row=1,column=0,columnspan=2)
@@ -79,13 +167,13 @@ class MainWindow(tk.Frame):
         # Get a list of avaiblible mods 
         self.avalible_mods = {}
         try:
-            for mod in os.listdir(mod_folder):
+            for mod in os.listdir(mod_dir):
                 #print(f"Possible mod folder: {mod}") 
-                #print(os.path.join(mod_folder,mod))
-                if os.path.isdir(os.path.join(mod_folder,mod)): # Mods need to be directories
-                    #print(f"Items inside of {mod}: {os.listdir(os.path.join(mod_folder,mod))}")
-                    for possible_raw_folder in os.listdir(os.path.join(mod_folder,mod)): 
-                        if os.path.isdir(os.path.join(mod_folder,mod,possible_raw_folder)) and possible_raw_folder == "raw": # raw folder inside the mod folder contains everything we need
+                #print(os.path.join(mod_dir,mod))
+                if os.path.isdir(os.path.join(mod_dir,mod)): # Mods need to be directories
+                    #print(f"Items inside of {mod}: {os.listdir(os.path.join(mod_dir,mod))}")
+                    for possible_raw_folder in os.listdir(os.path.join(mod_dir,mod)): 
+                        if os.path.isdir(os.path.join(mod_dir,mod,possible_raw_folder)) and possible_raw_folder == "raw": # raw folder inside the mod folder contains everything we need
                             # We now this directory inside of Mods contains a directory named "raw", so it seems to be a valid mod
                             # Add it to the modlist, if it isn't already there
                             try:
@@ -102,12 +190,12 @@ class MainWindow(tk.Frame):
             
                             if mod not in mod_info["Loaded Mods"]:
                                 self.availible_mods_listbox.insert(tk.END,mod)
-                                self.avalible_mods[mod] = os.path.join(os.path.join(mod_folder,mod))  
+                                self.avalible_mods[mod] = os.path.join(os.path.join(mod_dir,mod))  
                                 #print(f"Found raw folder in {mod}")
 
         except FileNotFoundError: # Generally this means there is no mod folder.
-            tkinter.messagebox.showerror("Error",'Mods folder not found! Please create a "mods" folder inside of the DF folder!')
-            raise SystemExit
+            tkinter.messagebox.showerror("Error",'Mods folder not found! Please create a "mods" folder and select it in the settings!')
+            raise SystemExit # What sys.exit does, without importing sys
 
 
     def load_mod(self,mod=False):
@@ -119,7 +207,7 @@ class MainWindow(tk.Frame):
                 tkinter.messagebox.showerror("Error","No mod selected to install!")
                 return
 
-        merge_dirs(os.path.join(mod_folder,mod,"raw"),os.path.join(df_dir,"raw"))# Add all files in the mod folder to the DF raws
+        merge_dirs(os.path.join(mod_dir,mod,"raw"),os.path.join(df_dir,"raw"))# Add all files in the mod folder to the DF raws
         
         with open(os.path.join(df_dir,"mod_info.json"), "r") as f:
             mod_info = json.load(f)
